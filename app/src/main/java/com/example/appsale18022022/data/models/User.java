d70d9b44d388add8d4a0cb5f2a89344b8794d8db
Phat <phatdroid94@gmail.com>
2022-05-20 19:21:59 +0700

    model

---
+++ app/src/main/java/com/example/appsale18022022/data/models/User.java
@@ -0,0 +1,49 @@
+package com.example.appsale18022022.data.models;
+
+public class User {
+    private String email;
+    private String name;
+    private String phone;
+    private Integer userGroup;
+    private String registerDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getUserGroup() {
+        return userGroup;
+    }
+
+    public void setUserGroup(Integer userGroup) {
+        this.userGroup = userGroup;
+    }
+
+    public String getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(String registerDate) {
+        this.registerDate = registerDate;
+    }
+}
