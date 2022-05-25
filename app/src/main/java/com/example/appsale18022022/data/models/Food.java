932ff1443a78ba68573a2bf2008158a9a0d12911
Phat <phatdroid94@gmail.com>
2022-05-25 18:20:49 +0700

    food

---
+++ app/src/main/java/com/example/appsale18022022/data/models/Food.java
@@ -0,0 +1,85 @@
+package com.example.appsale18022022.data.models;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class Food {
+
+    @SerializedName("_id")
+    private String id;
+    private String name;
+    private String address;
+    private int price;
+    private String img;
+    private int quantity;
+    private List<String> gallery;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<String> getGallery() {
+        return gallery;
+    }
+
+    public void setGallery(List<String> gallery) {
+        this.gallery = gallery;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", price=" + price +
+                ", img='" + img + '\'' +
+                ", quantity=" + quantity +
+                ", gallery=" + gallery +
+                '}';
+    }
+}
--- app/src/main/java/com/example/appsale18022022/presentation/views/home/HomeActivity.java
+++ app/src/main/java/com/example/appsale18022022/presentation/views/home/HomeActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.appsale18022022.R;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
