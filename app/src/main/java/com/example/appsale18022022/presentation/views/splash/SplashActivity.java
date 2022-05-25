abfb08fd7baa4c7e83e09e7116ab311a0560db91
Phat <phatdroid94@gmail.com>
2022-05-25 18:45:53 +0700

    splash screen

---
+++ app/src/main/java/com/example/appsale18022022/presentation/views/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsale18022022.presentation.views.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appsale18022022.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
